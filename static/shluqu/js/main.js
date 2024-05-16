52457a8d405d61570cb4fe79555ab73d39743e4e
test <138377120+residentialproxies@users.noreply.github.com>
2024-05-16 15:54:56 +0800

    Update main.js

--- static/shluqu/js/main.js
+++ static/shluqu/js/main.js
@@ -30,22 +30,6 @@
         }
       });
     })
-    //Faq
-    $('.faq--area .faq-title').on('click', function (e) {
-      var element = $(this).parent('.faq--item');
-      if (element.hasClass('open')) {
-        element.removeClass('open');
-        element.find('.faq-content').removeClass('open');
-        element.find('.faq-content').slideUp(300, "swing");
-      } else {
-        element.addClass('open');
-        element.children('.faq-content').slideDown(300, "swing");
-        element.siblings('.faq--item').children('.faq-content').slideUp(300, "swing");
-        element.siblings('.faq--item').removeClass('open');
-        element.siblings('.faq--item').find('.faq-title').removeClass('open');
-        element.siblings('.faq--item').find('.faq-content').slideUp(300, "swing");
-      }
-    });
 
     //Faq2 model
     document.addEventListener("DOMContentLoaded", function() {
